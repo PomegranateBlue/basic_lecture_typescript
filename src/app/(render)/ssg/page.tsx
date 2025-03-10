c1e22f3d7c931bad4e1a929235ce4b660b9f05af
bluepome98 <wpdn1998@gmail.com>
2025-03-10 20:06:37 +0900

    SSG 컴포넌트 생성

---
+++ src/app/(render)/ssg/page.tsx
@@ -0,0 +1,12 @@
+import { Todo } from "@/types/todo.type";
+
+const SSGPage = async () => {
+  const response = await fetch("https://localhost:5000/todos", {
+    cache: "force-cache",
+  });
+  const data: Todo[] = await response.json();
+
+  return <div>SSGPage:{JSON.stringify(data)}</div>;
+};
+
+export default SSGPage;
