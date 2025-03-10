31e84a9417700b75984421bb50c0bd8b4ccf3654
bluepome98 <wpdn1998@gmail.com>
2025-03-10 19:58:02 +0900

    todo 오류 해결

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
-const DetailPage = async() => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+const DetailPage = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   const todo = await data.json();
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import Link from "next/link";
-import { getTodos } from "@/api/todo-api";
 
-const HomePage = async () => {
-  const todos = await getTodos();
+const HomePage = () => {
   return (
     <div>
-      <h1>HomePage</h1>
+      HomePage
       <Link href="/detail">Detail Page</Link>
-      <ul>
-        {todos.map(({ id, text, completed }) => (
-          <li key={id}>
-            {text} {completed ? "(Completed)" : "(Pending)"}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
