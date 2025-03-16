ca0cab7e811980b234f37c7089e5688ab220942a
bluepome98 <wpdn1998@gmail.com>
2025-03-16 12:50:14 +0900

    shadcn 설치 및 투두 헤더 파일 설정

---
+++ src/app/components/todo/layout/TodoHeader.tsx
@@ -0,0 +1,11 @@
+const TodoHeader = () => {
+  return (
+    <header className="mb-4">
+      <div className="container mx-auto p-2">
+        <h1 className="text-4xl font-bold">Todo List</h1>
+      </div>
+    </header>
+  );
+};
+
+export default TodoHeader;
