f6dae4a34f00927b647bc875b484b2eb5f4d6f84
bluepome98 <wpdn1998@gmail.com>
2025-03-16 13:02:10 +0900

    버튼, 인풋 shadcn으로부터 설치 후, TodoForm에서 적용

--- src/app/components/todo/TodoForm.tsx
+++ src/app/components/todo/TodoForm.tsx
@@ -2,7 +2,8 @@
 
 import { createTodo } from "@/api/todo-api";
 import { FormEvent } from "react";
-
+import { Button } from "@/app/ui/button";
+import { Input } from "@/app/ui/input";
 const TodoForm = () => {
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -16,9 +17,18 @@ const TodoForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name="todo-next" required />
-      <button type="submit">추가</button>
+    <form
+      onSubmit={onSubmit}
+      className="flex w-full items-center space-x-2 bg-gray-200 p-3 rounded-md"
+    >
+      <Input
+        type="text"
+        name="todo-next"
+        required
+        placeholder="일정을 입력하세요"
+        className="bg-white"
+      />
+      <Button type="submit">추가하기</Button>
     </form>
   );
 };
