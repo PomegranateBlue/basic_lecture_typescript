96413fd06ae3d6021b003bd71539556407ecfa9e
bluepome98 <wpdn1998@gmail.com>
2025-03-18 20:05:49 +0900

    TodoList.tsx 수정, useQuery를 통해 데이터 페칭

--- src/app/components/todo/TodoList.tsx
+++ src/app/components/todo/TodoList.tsx
@@ -1,27 +1,13 @@
 "use client";
 
 import TodoItem from "./TodoItem";
-import { useState, useEffect } from "react";
-import { Todo } from "@/types/todo.type";
+import { useTodoQuery } from "@/app/query/useTodoQuery";
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const { data: todos } = useTodoQuery();
 
-  useEffect(() => {
-    const fetchTodos = async () => {
-      const response = await fetch("/api/todos", {
-        next: {
-          tags: ["todos"],
-        },
-      });
-      const data: Todo[] = await response.json();
-      setTodos(data);
-    };
-
-    fetchTodos();
-  }, []);
   return (
     <ul className="splace-y-2">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id}>
           <TodoItem todo={todo} />
         </li>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TodoHeader from "./components/todo/layout/TodoHeader";
+import Providers from "./provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <TodoHeader />
-        {children}
+        <Providers> {children}</Providers>
       </body>
     </html>
   );
