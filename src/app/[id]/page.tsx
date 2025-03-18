8d3b7db5977f4fc39b798cfc3aada805856ffac6
bluepome98 <wpdn1998@gmail.com>
2025-03-18 23:31:33 +0900

    SSR 렌더링을 통한 Prefetching 구현

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,22 +1,35 @@
 import TodoDetail from "../components/todo/TodoDetail";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { getTodoItem } from "@/api/todo-api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 interface DetailPageProps {
   params: Promise<{ id: string }>;
 }
 
 const DetailPage = async ({ params }: DetailPageProps) => {
   const { id } = await params;
+  const queryClient = new QueryClient();
 
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: () => getTodoItem(id),
+  });
   return (
-    <section>
-      <div className="container p-2 mx-auto space-y-4">
-        <TodoDetail id={id} />
-        <Link href="/">
-          <Button className="w-full">돌아가기</Button>
-        </Link>
-      </div>
-    </section>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <section>
+        <div className="container p-2 mx-auto space-y-4">
+          <TodoDetail id={id} />
+          <Link href="/">
+            <Button className="w-full">돌아가기</Button>
+          </Link>
+        </div>
+      </section>
+    </HydrationBoundary>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
+import { getTodos } from "@/api/todo-api";
 import TodoList from "./components/todo/TodoList";
 import TodoForm from "./components/todo/TodoForm";
-const HomePage = () => {
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+const HomePage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
   return (
-    <section>
-      <div className="container p-2 mx-auto space-y-4">
-        <TodoForm />
-        <TodoList />
-      </div>
-    </section>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <section>
+        <div className="container p-2 mx-auto space-y-4">
+          <TodoForm />
+          <TodoList />
+        </div>
+      </section>
+    </HydrationBoundary>
   );
 };
 
